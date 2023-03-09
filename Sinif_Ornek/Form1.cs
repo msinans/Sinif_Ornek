1ea1d50b2c35019116fe9b93de7f732b4b59ea54
m.sinans <msomercikoglu@gmail.com>
2023-03-09 11:09:18 +0300

    Add project files.

---
+++ Sinif_Ornek/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinif_Ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Araba araba = new Araba();
+            araba.renk = "Beyaz";
+            araba.hiz = 200;
+            araba.motor = 1249.34;
+            araba.fiyat = 550000;
+            araba.durum = 's';
+
+            label1.Text = araba.renk;
+            label2.Text = araba.hiz.ToString();
+            label3.Text = araba.motor.ToString();
+            label4.Text = araba.fiyat.ToString();
+            label5.Text = araba.durum.ToString();
+            pictureBox1.BackColor = Color.Bisque;
+
+        }
+    }
+}
---
+++ Sinif_Ornek/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sinif_Ornek
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Araba rb = new Araba();
+            rb.renk = "Siyah";
+            rb.hiz = 200;
+            rb.motor = 1499.45;
+            rb.fiyat = 250000;
+            rb.durum = 'i';
+
+            label1.Text = rb.renk;
+            label2.Text = rb.hiz.ToString();
+            label3.Text = rb.motor.ToString();
+            label4.Text = rb.fiyat.ToString();
+            label5.Text = rb.durum.ToString();
+            pictureBox1.BackColor = Color.Brown;
+        }
+    }
+}
